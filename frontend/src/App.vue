812f528a95cf0da5819c98e3f4af49f19a7f5e96
David Segerbo <david.k.segerbo@gmail.com>
2026-09-10 03:44:37 +0200

    setup for the base frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+
+<template>
+  <header>
+    <div class="wrapper">
+
+
+    </div>
+  </header>
+
+  <RouterView />
+
+</template>
+
+<style scoped>
+
+</style>
